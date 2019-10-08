69bf014de19a9dcc61309328fbd5375af0bb1971
David <haydal.tmps@gmail.com>
2019-10-08 22:49:33 +0100

    home.html added, app.py updated

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'kerry_rivers'
+app.config["MONGO_URI"] = 'mongodb+srv://haydal810:Glencar234@myfirstcluster-me2gf.mongodb.net/kerry_rivers?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
-def hello():
-    return "Hello World"
+@app.route("/get_river_names")
+def get_river_names():
+    return render_template("home.html", rivers=mongo.db.river_names.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
