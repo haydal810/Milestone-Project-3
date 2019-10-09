bf76783afff91137c99fbd49fba0f1f833109ab7
David <haydal.tmps@gmail.com>
2019-10-09 15:37:27 +0100

    added import env to app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import env
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -7,7 +8,7 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'kerry_rivers'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', "Env Not loaded")
 
 mongo = PyMongo(app)
 
