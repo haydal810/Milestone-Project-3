dc43343911b217a02ba33b4d967040465b79bdb6
David <haydal.tmps@gmail.com>
2019-10-11 14:40:01 +0100

    edit task function added to app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-## import env
+import env
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -12,11 +12,21 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', "Env Not loaded")
 
 mongo = PyMongo(app)
 
-
+## This is the function for the home page:
 @app.route("/")
 @app.route("/get_river_names")
 def get_river_names():
     return render_template("rivers.html", rivers=mongo.db.river_names.find())
+
+## This is the function for the edit river page:
+@app.route("/")
+@app.route("/edit_river")
+def edir_river():
+    return render_template("edit_river.html")
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'), 
