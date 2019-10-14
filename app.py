3e8256f3605d34e9cf2884be61201d1046f7b5a7
David <haydal.tmps@gmail.com>
2019-10-14 16:30:41 +0100

    fixed bug on app.py line 50

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def add_new_river():
 @app.route("/insert_river", methods=['POST'])
 def insert_river():
     rivers = mongo.db.river_names
-    rivers.insert_one(request.form.to_dic())
+    rivers.insert_one(request.form.to_dict())
     return redirect(url_for('get_river_names'))
 
 
