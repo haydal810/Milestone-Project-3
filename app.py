7441bf16251d52f563c80016aae77d48a1df4a46
David <haydal.tmps@gmail.com>
2019-10-16 16:49:33 +0100

    more tidy up on app.py and home template

--- app.py
+++ app.py
@@ -14,16 +14,24 @@ mongo = PyMongo(app)
 
 
 
-## This is the function for the HOME page:
+## HOME page:
+
 @app.route("/")
 @app.route("/get_river_names")
 def get_river_names():
     return render_template("home.html", rivers=mongo.db.river_names.find())
 
+## HOME page function - to show individual document in collection in db...
+
+@app.route("/show_river_doc")
+def show_river_doc():
+    return render_template("home.html", river_doc=mongo.db.river_names.find_one({"river_name" : request.form.get('river_name')}))
 
 
 
-## This is the function for the add_river page:
+
+
+## add_river page:
 
 @app.route("/add_new_river")
 def add_new_river():
@@ -33,13 +41,13 @@ def add_new_river():
 
 @app.route("/insert_river", methods=['POST'])
 def insert_river():
-    rivers = mongo.db.river_names
-    rivers.insert_one(request.form.to_dict())
+    river = mongo.db.river_names
+    river.insert_one(request.form.to_dict())
     return redirect(url_for('get_river_names'))
 
 
 
-## This is the function for the edit river page:
+## edit river page:
 
 @app.route("/edit_river")
 def edit_river():
@@ -47,14 +55,14 @@ def edit_river():
 
 
 
-## This is the function for the leave a review page:
+## leave a review page:
 
 @app.route("/leave_review_river")
 def leave_review_river():
     return render_template("leave_review.html", rivers=mongo.db.river_names.find())
 
 
-## This is the function for the river_review page:
+## read reviews page:
 
 @app.route("/read_review")
 def read_review():
