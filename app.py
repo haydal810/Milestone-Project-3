4f891d766f839f79b40064ff4898c15d55bad12b
David <haydal.tmps@gmail.com>
2019-10-19 00:26:02 +0100

    update_ fixed & update route in app.py defined

--- app.py
+++ app.py
@@ -12,26 +12,29 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', "Env Not loaded")
 mongo = PyMongo(app)
 
 
-## HOME page:
+# HOME page:
 
 @app.route("/")
 @app.route("/get_river_names")
 def get_river_names():
     return render_template("home.html", rivers=mongo.db.river_names.find())
 
-## ABOUT page:
+# ABOUT page:
+
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-## ADD_RIVER page:
+# ADD_RIVER page:
+
 
 @app.route("/add_new_river")
 def add_new_river():
     return render_template("add_river.html", rivers=mongo.db.river_names.find())
 
-## This function POSTs the input data from the user, to the database:
+# This function POSTs the input data from the user, to the database:
+
 
 @app.route("/insert_river", methods=['POST'])
 def insert_river():
@@ -40,8 +43,7 @@ def insert_river():
     return redirect(url_for('get_river_names'))
 
 
-
-## UPDATE_RIVER page:
+# UPDATE_RIVER page:
 
 @app.route("/edit_river/<river_id>")
 def edit_river(river_id):
@@ -49,31 +51,33 @@ def edit_river(river_id):
     return render_template("update_river.html", river=the_river)
 
 
+@app.route("/update_river/<river_id>", methods=["POST"])
+def update_river(river_id):
+    river = mongo.db.river_names
+    river.update({'_id': ObjectId(river_id)}, {
+        'river_name' : request.form.get('river_name'),
+        'status' : request.form.get('status'),
+        'fish' : request.form.get('fish'),
+        'free_or_permit' : request.form.get('free_or_permit'),
+        'angling_methods' : request.form.get('angling_methods')
+    })
+    return redirect(url_for('get_river_names'))
 
 
-## leave a review page:
+
+# leave a review page:
+
 
 @app.route("/leave_review_river")
 def leave_review_river():
     return render_template("leave_review.html", rivers=mongo.db.river_names.find())
 
 
-## read reviews page:
+# read reviews page:
 
 @app.route("/read_review")
 def read_review():
     return render_template("read_review.html", rivers=mongo.db.river_names.find_one())
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
